8a968c2941b4e0a63706b11f878a95919da910f1
Jordan Irgang <jordanirgang@gmail.com>
2024-11-17 21:01:22 -0500

    removed soft links in place of submodule

--- add_thirdparty_libs.sh
+++ add_thirdparty_libs.sh
@@ -1,3 +1,4 @@
 mkdir -p ./lib/thirdparty/
+
 find ./lib/ -iwholename "*lib*lib*.h"| xargs -I % sh -c "export name='basename %'; cp % ./lib/thirdparty/$name"
-find ./lib/ -iwholename "*lib*lib*.cpp"| xargs -I % sh -c "basename %"
+find ./lib/ -iwholename "*lib*lib*.cpp"| xargs -I % sh -c "export name='basename %'; cp % ./lib/thirdparty/$name"
