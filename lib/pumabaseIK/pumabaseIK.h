11f9cbeb42ec0c3af93fe640a527ef31c3386282
Jordan Irgang <jordanirgang@gmail.com>
2024-11-19 01:57:30 -0500

    decsion for chosing nodes added

--- lib/pumabaseIK/pumabaseIK.h
+++ lib/pumabaseIK/pumabaseIK.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <Arduino.h>
+
+#define RADIANS * (180/M_PI)
 namespace generic{
 	struct cartesian{
 		float x;
@@ -46,8 +48,85 @@ namespace generic{
 	//	theta_1_solve = sp.atan2(z,sp.sqrt(x**2+y**2-offset**2))-sp.atan2(length_c*sp.sin(theta_2),length_b+length_c*sp.cos(theta_2))
 		jointsOUT->theta2SlnA = atan2(coordIN->z,sqrt(pow(coordIN->x,2)+pow(coordIN->y,2)-pow(linksIN->offsetYAxisA1A2,2))
 										- atan2(linksIN->a3*sin(jointsOUT->theta3SlnA),linksIN->a2+linksIN->a3*cos(jointsOUT->theta3SlnA)));
-		jointsOUT->theta2SlnA = atan2(coordIN->z,sqrt(pow(coordIN->x,2)+pow(coordIN->y,2)-pow(linksIN->offsetYAxisA1A2,2))
+		jointsOUT->theta2SlnB = atan2(coordIN->z,sqrt(pow(coordIN->x,2)+pow(coordIN->y,2)-pow(linksIN->offsetYAxisA1A2,2))
 		- atan2(linksIN->a3*sin(jointsOUT->theta3SlnB),linksIN->a2+linksIN->a3*cos(jointsOUT->theta3SlnB)));
 		
+	}
+
+
+	void radian2deg(jointSpace *jointsOUT){
+		jointsOUT->theta1SlnA = fmod(jointsOUT->theta1SlnA RADIANS,360);
+		jointsOUT->theta1SlnB = fmod(jointsOUT->theta1SlnB RADIANS,360);
+		jointsOUT->theta2SlnA = fmod(jointsOUT->theta2SlnA RADIANS,360);
+		jointsOUT->theta2SlnB = fmod(jointsOUT->theta2SlnB RADIANS,360);
+		jointsOUT->theta3SlnA = fmod(jointsOUT->theta3SlnA RADIANS,360);
+		jointsOUT->theta3SlnB = fmod(jointsOUT->theta3SlnB RADIANS,360);
+	}
+
+	float chosetheta1(jointSpace *jointsOUT,float restPoint){
+		//choose closest to origin
+		//return fmin(abs(restPoint-jointsOUT->theta1SlnA),abs(restPoint-jointsOUT->theta1SlnB));
+		float defaultOut = 0;
+
+		if(!isinf(jointsOUT->theta2SlnA) && !isinf(jointsOUT->theta3SlnA)){
+			if(abs(restPoint-jointsOUT->theta1SlnA) >= abs(restPoint-jointsOUT->theta1SlnB)){
+				defaultOut = jointsOUT->theta1SlnB - restPoint;
+			}else{
+				defaultOut = jointsOUT->theta1SlnA - restPoint;
+			}
+		}
+
+		return defaultOut;
+	}
+	
+	float chosetheta2(jointSpace *jointsOUT,float restPoint){
+		//choose closest to origin
+		//return fmin(abs(restPoint-jointsOUT->theta1SlnA),abs(restPoint-jointsOUT->theta1SlnB));
+		float defaultOut = 0;
+
+
+		if((abs(restPoint-jointsOUT->theta3SlnA) >= abs(restPoint-jointsOUT->theta3SlnB)) && 
+			!(isinf(jointsOUT->theta2SlnA) || isinf(jointsOUT->theta3SlnA || isinf(jointsOUT->theta2SlnB) || isinf(jointsOUT->theta3SlnB)))
+		){
+			defaultOut = jointsOUT->theta2SlnB- restPoint;
+		}else if(!(isinf(jointsOUT->theta2SlnA) || isinf(jointsOUT->theta3SlnA) || isinf(jointsOUT->theta2SlnB) || isinf(jointsOUT->theta3SlnB))){
+			defaultOut = jointsOUT->theta2SlnA- restPoint;
+		}
+
+		if((isinf(jointsOUT->theta2SlnA) || isinf(jointsOUT->theta3SlnA)) && !(isinf(jointsOUT->theta2SlnB) || isinf(jointsOUT->theta3SlnB))){
+			defaultOut =  jointsOUT->theta2SlnB- restPoint;
+		}
+
+		if((isinf(jointsOUT->theta2SlnB) || isinf(jointsOUT->theta3SlnB)) && !(isinf(jointsOUT->theta2SlnA) || isinf(jointsOUT->theta3SlnA))){
+			defaultOut =  jointsOUT->theta2SlnA - restPoint;
+		}
+
+		return defaultOut;S
+
+	}
+
+	float chosetheta3(jointSpace *jointsOUT,float restPoint){
+		//choose closest to origin
+		//return fmin(abs(restPoint-jointsOUT->theta1SlnA),abs(restPoint-jointsOUT->theta1SlnB));
+		float defaultOut = 0;
+
+
+		if((abs(restPoint-jointsOUT->theta3SlnA) >= abs(restPoint-jointsOUT->theta3SlnB)) && 
+			!(isinf(jointsOUT->theta2SlnA) || isinf(jointsOUT->theta3SlnA || isinf(jointsOUT->theta2SlnB) || isinf(jointsOUT->theta3SlnB)))
+		){
+			defaultOut = jointsOUT->theta3SlnB - restPoint;
+		}else if(!(isinf(jointsOUT->theta2SlnA) || isinf(jointsOUT->theta3SlnA) || isinf(jointsOUT->theta2SlnB) || isinf(jointsOUT->theta3SlnB))){
+			defaultOut = jointsOUT->theta3SlnA - restPoint;
+		}
+
+		if((isinf(jointsOUT->theta2SlnA) || isinf(jointsOUT->theta3SlnA)) && !(isinf(jointsOUT->theta2SlnB) || isinf(jointsOUT->theta3SlnB))){
+			defaultOut =  jointsOUT->theta3SlnB - restPoint;
+		}
+
+		if((isinf(jointsOUT->theta2SlnB) || isinf(jointsOUT->theta3SlnB)) && !(isinf(jointsOUT->theta2SlnA) || isinf(jointsOUT->theta3SlnA))){
+			defaultOut = jointsOUT->theta3SlnA - restPoint;
+		}
+
+		return defaultOut;
 	}
 }
